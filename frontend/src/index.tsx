ee0a6f1bb047a1401756006151085cc67ef38543
Jared Turck <jaredturck8@gmail.com>
2026-01-06 01:33:21 +0000

    Changed config to work with github pages

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { ScrollToTop } from './components/scroll_to_top';
@@ -8,9 +8,12 @@ import { ScrollToTop } from './components/scroll_to_top';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const basename = process.env.PUBLIC_URL && process.env.PUBLIC_URL !== "." ? process.env.PUBLIC_URL : undefined;
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <ScrollToTop />
       <App />
     </BrowserRouter>
